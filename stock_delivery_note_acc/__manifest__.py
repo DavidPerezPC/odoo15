788b55f61a49332fe883587556ee345083502a0a
erickabrego <77135099+erickabrego@users.noreply.github.com>
2021-10-21 12:17:40 -0600

    Update __manifest__.py

--- stock_delivery_note_acc/__manifest__.py
+++ stock_delivery_note_acc/__manifest__.py
@@ -27,14 +27,17 @@
         "web.assets_backend": [
             "stock_delivery_note_acc/static/src/css/margin_form.css",
             "stock_delivery_note_acc/static/src/css/invoice-style.css",
-        ]
+        ],
+        "web.report_assets_common": [
+            "stock_delivery_note_acc/static/src/less/fonts.less",
+        ],
     },
 
     'data': [
         # 'views/item_web.xml',
         'reports/delivery_note_report_acc.xml',
         'reports/delivery_note_report_view.xml',
-        'views/styles_css.xml',
+        #'views/styles_css.xml',
 
     ],
 
